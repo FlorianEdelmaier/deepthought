54bb457a674499ccee49b183ee48d5c49a56f87f
Florian Edelmaier <florian.edelmaier@gmail.com>
2017-08-16 18:33:53 +0200

    experimental change

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ const Tokenizer = require('./lexer');
 const parser = require('./parser');
 
 let tokens = new Tokenizer('let test = (1 + 4) * 3').tokenize();
+// console.log(...tokens);
+// console.log("=======================");
 let ast = parser(tokens);
-console.log(ast);
+ast.body.forEach(function(node) {
+    console.log(node.value)
+}, this);
+// console.log(ast);
